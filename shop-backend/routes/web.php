1920b33d18fea4bc660049fe7d2be3b0670f4bff
naveedmojo <naveedmojojo@gmail.com>
2025-03-17 12:49:26 +0530

    delete

--- shop-backend/routes/web.php
+++
@@ -1,8 +0,0 @@
-<?php
-use App\Http\Controllers\ProductController;
-use Illuminate\Support\Facades\Route;
-
-Route::prefix('api/v1')->group(function () {
-    Route::get('/products', [ProductController::class, 'index']);
-});
-
